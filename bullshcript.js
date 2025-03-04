4a279cf3c2d23d666715ea069df6f5786c62ec50
Vanquish3r <153280214+vanquish3r@users.noreply.github.com>
2025-03-04 04:40:56 -0700

    Update bullshcript.js Moved toggles to Unity VS

--- bullshcript.js
+++ bullshcript.js
@@ -6,18 +6,20 @@
 /* Old Screencast - https://sidequestvr.github.io/SideQuest.Banter.Spaces/screen-cast/?sid=42424242 */
 /* YouTube Live - https://www.youtube.com/embed/EF0-j9ORe2Y?autoplay=1&controls=0 */
 let youtubePlaylist = `PL24rMr-iJJ1LwIG3SPYtnJa3qd3O4F-oQ`;
-let websiteurl = "https://sidequestvr.github.io/SideQuest.Banter.Spaces/screen-cast/?sid=42424242"; /* ?autoplay=1&controls=0 For YouTube Live */ 
+// let websiteurl = "https://sidequestvr.github.io/SideQuest.Banter.Spaces/screen-cast/?sid=42424242"; /* ?autoplay=1&controls=0 For YouTube Live */ 
 let otherwebsiteurl = "https://firer.at/pages/games.html";
 
-BS.BanterScene.GetInstance().On("loaded", () => {
-	console.log("Scene loaded");
+
+// BS.BanterScene.GetInstance().On("loaded", () => {
+// 	console.log("Scene loaded");
 	/* UNCOMMENTED THIS TO ENABLE THE YOUTUBE PLAYER */
 		//  enableYouTube();
 	/* UNCOMMENTED THIS TO ENABLE SCREEN CAST / YOUTUBE LIVE */
-		 enableTheFireScreen();
+		// enableTheFireScreen();
 	/* UNCOMMENTED THIS TO ENABLE FIRE TABLET */
-		 enableThePortableFireScreen();
-});
+		// enableThePortableFireScreen();
+// });
+
 
 // videoplayer toggle by HBR & FireRat
 let ytplayerdisabled = true;
